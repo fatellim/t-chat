ef26aa0f18fb0060272f2a38d00793c0bf8d5117
Cevantime <thibaulttruffert@hotmail.com>
2016-11-18 15:28:53 +0100

    Installation du framework W!

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -4,14 +4,12 @@ namespace Controller;
 
 use \W\Controller\Controller;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
 
 	/**
 	 * Page d'accueil par défaut
 	 */
-	public function home()
-	{
+	public function home() {
 		$this->show('default/home');
 	}
 
