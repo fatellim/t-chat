a98a6afc694d4016571d61f8c16e066d854fc053
= <=>
2016-11-30 18:26:08 +0100

    fix commit foireux

--- app/Controller/SalonController.php
+++ app/Controller/SalonController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace Controller;
@@ -59,41 +58,5 @@ class SalonController extends BaseController
 		
 		$this->show('salons/newmessages', array('messages' => $messages));
 	}
-=======
-<?php
 
-namespace Controller;
-
-use Model\SalonsModel;
-use Model\MessagesModel;
-
-class SalonController extends BaseController
-{
-	/**
-	 * Cette action permet de voir la liste des messages d'un salon
-	 * @param int $id l'id du salon dont je cherche à voir les messages
-	 */
-	public function seeSalon($id) {
-		/*
-		 * On instancie le modèle des salons de façon à récupérer les informations
-		 * du salon dont l'id est $id (passé dans l'url)
-		 */
-		$salonsModel = new SalonsModel();
-		$salon = $salonsModel->find($id);
-		
-		/*
-		 * On instancie le modèles des messages pour récupérer les messages du
-		 * salon dont l'id est $id
-		 */
-		$messagesModel = new MessagesModel();
-		
-		/*
-		 * J'utilise une méthode propre au modèle MessagesModel qui permet de 
-		 * récupérer les messages avec les infos utilisateur associées
-		 */
-		$messages = $messagesModel->searchAllWithUserInfos($id);
-		
-		$this->show('salons/see', array('salon' => $salon, 'messages' => $messages));
-	}
->>>>>>> 3890c2e56029f266f4b487673381e731ba5ef89f
 }
--- app/Model/MessagesModel.php
+++ app/Model/MessagesModel.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 
@@ -45,37 +44,3 @@ class MessagesModel extends Model {
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 }
-=======
-<?php
-
-
-namespace Model;
-
-use \PDO;
-use \W\Model\Model;
-
-/**
- * Description of MessagesModel
- *
- * @author Admin
- */
-class MessagesModel extends Model {
-	
-	/**
-	 * Cette fonction sélectionne tous les messages d'un salon en les associant
-	 * avec les infos de leur utilisateur respectif
-	 * @param int $idSalon l'id du salon dont on souhaite récupérer les messages
-	 * @return array la liste des messages avec les infos utilisateur
-	 */
-	public function searchAllWithUserInfos($idSalon) {
-		$query = "SELECT * FROM $this->table"
-				. " JOIN utilisateurs ON $this->table.id_utilisateur = utilisateurs.id"
-				. " WHERE id_salon = :id_salon";
-		
-		$stmt = $this->dbh->prepare($query);
-		$stmt->bindParam(':id_salon', $idSalon, PDO::PARAM_INT);
-		$stmt->execute();
-		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
-}
->>>>>>> 3890c2e56029f266f4b487673381e731ba5ef89f
